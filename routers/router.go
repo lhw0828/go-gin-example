c5c9faf6589f121c425d2e3b7d999db123f2f484
lhw <434962239@qq.com>
2024-09-14 15:49:07 +0800

    完成文章所有接口

--- routers/router.go
+++ routers/router.go
@@ -14,13 +14,20 @@ func InitRouter() *gin.Engine {
 
 	gin.SetMode(setting.RunMode)
 
-	apiv1 := r.Group("/api/v1")
+	apiV1 := r.Group("/api/v1")
 	{
+		// 文章标签相关
+		apiV1.GET("/tags", v1.GetTags)
+		apiV1.POST("/tags", v1.AddTag)
+		apiV1.PUT("/tags/:id", v1.EditTag)
+		apiV1.DELETE("/tags/:id", v1.DeleteTag)
 
-		apiv1.GET("/tags", v1.GetTags)
-		apiv1.POST("/tags", v1.AddTag)
-		apiv1.PUT("/tags/:id", v1.EditTag)
-		apiv1.DELETE("/tags/:id", v1.DeleteTag)
+		// 文章相关
+		apiV1.GET("/articles", v1.GetArticles)
+		apiV1.GET("/articles/:id", v1.GetArticle)
+		apiV1.POST("/articles", v1.AddArticle)
+		apiV1.PUT("/articles/:id", v1.EditArticle)
+		apiV1.DELETE("/articles/:id", v1.DeleteArticle)
 	}
 
 	r.GET("/test", func(c *gin.Context) {
